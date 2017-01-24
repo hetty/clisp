a0d7c2478e30016dad32ef5c5444b99353149718
hetty <870794106@qq.com>
2017-01-24 11:14:28 +0800

    增加chapter3

---
+++ chapter3.scm
@@ -0,0 +1,30 @@
+(+ 2 3)
+
+(odd? (+ 2 6))
+
+(+ 2 3)
+
+;===================================
+(define average (x y)
+  (/ (+ x y) 2.0))
+
+(average 3 7)
+
+
+(equal 5 6)
+
+(define square (n) (* n n))
+
+(define total-cost (quantity price handling-charge)
+  (+ (* quantity price) handling-charge))
+
+(equal 'kirl 'spock)
+
+(list 'james t 'kirk)
+
+(define riddle (x y)
+  (list 'why 'is 'a x 'like 'a y))
+
+(riddle 'raven 'writing-desk)
+
+
